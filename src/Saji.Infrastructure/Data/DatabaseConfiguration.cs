17c9cd1d3d337e8d0a8b3bc0dcfb37f471a6263b
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-04-21 03:30:05 +1000

    sample app

--- src/Saji.Infrastructure/Data/DatabaseConfiguration.cs
+++ src/Saji.Infrastructure/Data/DatabaseConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Saji.Infrastructure.Data;
@@ -9,37 +8,6 @@ namespace Saji.Infrastructure.Data;
 /// </summary>
 public static class DatabaseConfiguration
 {
-    /// <summary>
-    /// Migrates the databaes related to the DB context of type <typeparamref name="T"/>
-    /// </summary>
-    /// <typeparam name="T">
-    /// DB context to migrate
-    /// </typeparam>
-    /// <param name="builder">
-    /// Application buider
-    /// </param>
-    public static void MigrationDatabase<T>(this IApplicationBuilder builder)
-        where T : DbContext
-    {
-        if (builder?.ApplicationServices == null)
-        {
-            return;
-        }
-
-        var scopeFactory = builder.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
-
-        if (scopeFactory == null)
-        {
-            return;
-        }
-
-        using (var serviceScope = scopeFactory.CreateScope())
-        {
-            var dbContext = serviceScope.ServiceProvider.GetRequiredService<T>();
-            dbContext.Database.Migrate();
-        }
-    }
-
     /*
     public static void AddDatabaseHealthCheck(this IApplicationBuilder builder, string connectionString)
     {
