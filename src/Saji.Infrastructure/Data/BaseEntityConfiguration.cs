264408ea717015efdc9b1cf9a65187d8661ee43d
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-04-20 21:57:56 +1000

    started ef stuff

---
+++ src/Saji.Infrastructure/Data/BaseEntityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Saji.Domain;
+
+namespace Saji.Infrastructure.Data;
+
+/// <summary>
+/// Base Entity Configuration
+/// </summary>
+/// <typeparam name="TEntity">
+/// Entity type to be configured
+/// </typeparam>
+/// <typeparam name="TId">
+/// Entity key type
+/// </typeparam>
+public abstract class BaseEntityConfiguration<TEntity, TId> : IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseEntity<TId>
+{
+    /// <summary>
+    /// Configures the entity of type <see cref="IEntity{TId}"/>
+    /// </summary>
+    /// <param name="builder">
+    /// The builder to be used to configure the entity type
+    /// </param>
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.CreatedBy)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(i => i.ModifiedBy)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(i => i.Timestamp)
+            .IsRowVersion();
+
+        builder.Ignore(i => i.DomainEvents);
+    }
+}
---
+++ src/Saji.Infrastructure/Data/EnumEntityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Saji.Domain;
+
+namespace Saji.Infrastructure.Data;
+
+/// <summary>
+/// <see cref="IEnumEntity{TKey}"/> Configuration
+/// </summary>
+/// <typeparam name="TEnum">
+/// Enum type
+/// </typeparam>
+/// <typeparam name="TEnumEntity">
+/// Enum lookup table type
+/// </typeparam>
+public class EnumEntityConfiguration<TEnum, TEnumEntity> : IEntityTypeConfiguration<TEnumEntity>
+    where TEnum : struct, IConvertible, IComparable
+    where TEnumEntity : class, IEnumEntity<TEnum>
+{
+    /// <summary>
+    /// Configures enum entity lookup database table
+    /// </summary>
+    /// <param name="builder">
+    /// Table builder
+    /// </param>
+    public virtual void Configure(EntityTypeBuilder<TEnumEntity> builder)
+    {
+        if (builder is null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(i => i.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+    }
+}
