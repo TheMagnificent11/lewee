d970e786b0b2451c79f468adb85bc6daa4122338
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-04-22 16:26:21 +1000

    base app db context

---
+++ src/Saji.Infrastructure/Data/BaseApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Saji.Infrastructure.Data;
+
+/// <summary>
+/// Base Appliation Database Context
+/// </summary>
+public abstract class BaseApplicationDbContext : DbContext
+{
+    /// <summary>
+    /// Gets the database schema for the context
+    /// </summary>
+    protected abstract string Schema { get; }
+
+    /// <summary>
+    /// Configures the database
+    /// </summary>
+    /// <param name="modelBuilder">
+    /// Model builder
+    /// </param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        /* TODO: configure schema */
+
+        modelBuilder.ApplyConfiguration(new DomainEventReferenceConfiguration());
+    }
+}
