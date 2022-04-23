7cceac91547a902ec06eb2cbe7977d0da39cc51f
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-04-23 16:44:38 -0700

    started domain event dispatcher

---
+++ src/Saji.Infrastructure/Events/DomainEventDispatcher.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Saji.Infrastructure.Events;
+
+/// <summary>
+/// Domain Event Dispatcher
+/// </summary>
+/// <typeparam name="TContext">
+/// Databae context type
+/// </typeparam>
+public class DomainEventDispatcher<TContext>
+    where TContext : DbContext
+{
+    /// <summary>
+    /// Dispatch a domain events
+    /// </summary>
+    /// <param name="cancellationToken">
+    /// Cancellation toke
+    /// </param>
+    /// <returns>
+    /// An async task
+    /// </returns>
+    public Task DispatchEvents(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
