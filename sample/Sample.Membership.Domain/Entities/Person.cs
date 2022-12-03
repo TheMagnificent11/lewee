2ede440c1a6dd4962acb363316675a6276ef7bdc
Saji Weerasingham <saji.weerasingham@outlook.com>
2022-12-03 10:14:35 +1000

    started membership domain

---
+++ sample/Sample.Membership.Domain/Entities/Person.cs
@@ -0,0 +1,46 @@
+﻿using Saji.Domain;
+using Sample.Membership.Domain.ValueObjects;
+
+namespace Sample.Membership.Domain.Entities;
+
+public class Person : BaseEntity
+{
+    public Person(string givenName, string surname)
+    {
+        this.GivenName = givenName;
+        this.Surname = surname;
+    }
+
+    public Person(string givenName, string surname, DateOnly dateOfBirth)
+        : this(givenName, surname)
+    {
+        this.DateOfBirth = dateOfBirth;
+    }
+
+    public string GivenName { get; }
+    public string Surname { get; }
+    public DateOnly? DateOfBirth { get; private set; }
+    public string? EmailAddress { get; private set; }
+    public Address? Address { get; private set; }
+    public Address? PostalAddress { get; private set; }
+
+    public void SetEmailAddress(string emailAddress)
+    {
+        if (emailAddress.Equals(this.EmailAddress, StringComparison.InvariantCultureIgnoreCase))
+        {
+            return;
+        }
+
+        this.EmailAddress = emailAddress.ToLowerInvariant();
+    }
+
+    public void SetAddress(Address address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetPostalAddress(Address address)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ sample/Sample.Membership.Domain/ValueObjects/Address.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Membership.Domain.ValueObjects;
+
+public class Address
+{
+    public Address(
+        int streetNumber,
+        string streetName,
+        string streetType,
+        string locality,
+        string stateProvince,
+        string country,
+        string postcode)
+    {
+        this.StreetNumber = streetNumber;
+        this.StreetName = streetName;
+        this.StreetType = streetType;
+        this.Locality = locality;
+        this.StateProvince = stateProvince;
+        this.Country = country;
+        this.Postcode = postcode;
+    }
+
+    public Address(
+        string unit,
+        int streetNumber,
+        string streetName,
+        string streetType,
+        string locality,
+        string stateProvince,
+        string country,
+        string postcode)
+        : this(streetNumber, streetName, streetType, locality, stateProvince, country, postcode)
+    {
+        this.Unit = unit;
+    }
+
+    public int StreetNumber { get; }
+    public string StreetName { get; }
+    public string StreetType { get; }
+    public string Locality { get; }
+    public string StateProvince { get; }
+    public string Country { get; }
+    public string Postcode { get; }
+    public string? Unit { get; }
+}
