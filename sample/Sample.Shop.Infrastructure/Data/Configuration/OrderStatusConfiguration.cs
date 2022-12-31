3e9831356196649e74c7b029be71fc868848107e
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-01-01 05:16:11 +1000

    removed shop

--- sample/Sample.Shop.Infrastructure/Data/Configuration/OrderStatusConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿using Saji.Infrastructure.Data;
-using Sample.Shop.Domain.Entities;
-
-namespace Sample.Shop.Infrastructure.Data.Configuration;
-
-public sealed class OrderStatusConfiguration : BaseEnumEntityConfiguration<OrderStatus>
-{
-}
