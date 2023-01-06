062942338b1d871249df9a1536b8deb64a5d5f79
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-01-06 16:17:38 +1000

    orders domain

---
+++ sample/Sample.Orders.Domain/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lewee.Domain;
+
+namespace Sample.Orders.Domain;
+
+public class MenuItem : BaseEntity, IAggregateRoot
+{
+    public MenuItem(string name, decimal price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public string Name { get; protected set; }
+    public decimal Price { get; protected set; }
+}
---
+++ sample/Sample.Orders.Domain/Order.cs
@@ -0,0 +1,26 @@
+﻿using Lewee.Domain;
+
+namespace Sample.Orders.Domain;
+public class Order : BaseEntity, IAggregateRoot
+{
+    private readonly List<MenuItem> items;
+
+    public Order(int tableNumber)
+    {
+        this.items = new List<MenuItem>();
+
+        this.TableNumber = tableNumber;
+        this.Items = this.items;
+    }
+
+    public int TableNumber { get; protected set;  }
+
+    public IReadOnlyCollection<MenuItem> Items { get; protected set; }
+
+    public decimal Total => this.items.Sum(x => x.Price);
+
+    public void AddItem(MenuItem item)
+    {
+        this.items.Add(item);
+    }
+}
