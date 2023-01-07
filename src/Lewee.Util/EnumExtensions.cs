b2953073ddf893a3df6bac1c550dbd4cbd3d624d
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-01-08 03:09:17 +1000

    enum extensions

---
+++ src/Lewee.Util/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Lewee.Util;
+
+/// <summary>
+/// Enum Extension Methods
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// Gets the <see cref="DescriptionAttribute"/> description from an enum value if it exists, otherwise returns ToString() of the value
+    /// </summary>
+    /// <param name="value">Enum item</param>
+    /// <returns><see cref="DescriptionAttribute"/> description or ToString() of enum value</returns>
+    public static string GetDescription(this Enum value)
+    {
+        // Get the Description attribute value for the enum value
+        var field = value
+            .GetType()
+            .GetField(value.ToString());
+
+        var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+
+        return attribute?.Description ?? value.ToString();
+    }
+}
---
+++ tests/Lewee.Util.Tests.Unit/EnumExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using FluentAssertions;
+using Xunit;
+
+namespace Lewee.Util.Tests.Unit;
+
+public static class EnumExtensionTests
+{
+    public enum TableStatus
+    {
+        Available = 0,
+
+        Reserved = 1,
+
+        [Description("In Use")]
+        InUse = 2
+    }
+
+    [Theory]
+    [InlineData(TableStatus.Available, "Available")]
+    [InlineData(TableStatus.Reserved, "Reserved")]
+    [InlineData(TableStatus.InUse, "In Use")]
+    public static void GetDescription_ReturnCorrectDescription(TableStatus value, string expectedDescription)
+    {
+        var result = value.GetDescription();
+
+        result.Should().NotBeNull();
+        result.Should().Be(expectedDescription);
+    }
+}
