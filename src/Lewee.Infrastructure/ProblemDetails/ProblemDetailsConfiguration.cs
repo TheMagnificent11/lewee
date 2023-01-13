22d3ed13480d8c08af8df17fc22d91ea8c0f10e6
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-01-13 23:04:32 +1000

    more infrastructure

---
+++ src/Lewee.Infrastructure/ProblemDetails/ProblemDetailsConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lewee.Infrastructure.ProblemDetails;
+
+/// <summary>
+/// Problem Details Configuration
+/// </summary>
+public static class ProblemDetailsConfiguration
+{
+    /// <summary>
+    /// Configures problem details
+    /// </summary>
+    /// <param name="services">Services collection</param>
+    public static void ConfigureProblemDetails(this IServiceCollection services)
+    {
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.InvalidModelStateResponseFactory = context =>
+            {
+                var problemDetails = new ValidationProblemDetails(context.ModelState)
+                {
+                    Instance = context.HttpContext.Request.Path,
+                    Status = (int)HttpStatusCode.BadRequest,
+                    Detail = "Please refer to the errors property for additional details"
+                };
+
+                return new BadRequestObjectResult(problemDetails)
+                {
+                    ContentTypes = { "application/json" }
+                };
+            };
+        });
+    }
+}
