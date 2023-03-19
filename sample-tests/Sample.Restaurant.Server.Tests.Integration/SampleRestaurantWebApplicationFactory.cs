40643f0f26bba8219f80427781f58a01ecce3c8f
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-03-20 05:05:32 +1000

    started integration tests

---
+++ sample-tests/Sample.Restaurant.Server.Tests.Integration/SampleRestaurantWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Sample.Restaurant.Infrastructure.Data;
+
+namespace Sample.Restaurant.Server.Tests.Integration;
+
+public class SampleRestaurantWebApplicationFactory : WebApplicationFactory<Program>
+{
+    private readonly string connectionString = $"DataSource={Guid.NewGuid()}.db";
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<RestaurantDbContext>))
+                        ?? throw new InvalidOperationException("Could not find descriptor for DB context");
+
+            services.Remove(descriptor);
+
+            services.AddDbContext<RestaurantDbContext>(options => options.UseSqlite(this.connectionString));
+        });
+    }
+}
