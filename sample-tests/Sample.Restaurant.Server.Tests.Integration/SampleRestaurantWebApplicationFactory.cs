43cee0933de6f9f80b70afc265992acfb0012c58
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-03-20 12:21:07 +1000

    refactor

--- sample-tests/Sample.Restaurant.Server.Tests.Integration/SampleRestaurantWebApplicationFactory.cs
+++ sample-tests/Sample.Restaurant.Server.Tests.Integration/SampleRestaurantWebApplicationFactory.cs
@@ -1,27 +1,14 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Lewee.IntegrationTests;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Sample.Restaurant.Infrastructure.Data;
 
 namespace Sample.Restaurant.Server.Tests.Integration;
 
 public class SampleRestaurantWebApplicationFactory : WebApplicationFactory<Program>
 {
-    private readonly string connectionString = $"DataSource={Guid.NewGuid()}.db";
-
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(services =>
-        {
-            var descriptor = services.SingleOrDefault(
-                d => d.ServiceType ==
-                    typeof(DbContextOptions<RestaurantDbContext>))
-                        ?? throw new InvalidOperationException("Could not find descriptor for DB context");
-
-            services.Remove(descriptor);
-
-            services.AddDbContext<RestaurantDbContext>(options => options.UseSqlite(this.connectionString));
-        });
+        builder.ConfigureServices(services => services.ReplacedSqlServerWithSqlLite<RestaurantDbContext>());
     }
 }
---
+++ src/Lewee.IntegrationTests/WebApplictionFactoryConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lewee.IntegrationTests;
+
+/// <summary>
+/// Web Application Factory Configuration
+/// </summary>
+public static class WebApplictionFactoryConfiguration
+{
+    /// <summary>
+    /// REplaces the SQL server dependency injection service descriptor for <typeparamref name="T"/> with an SQL Lite
+    /// service descriptor
+    /// </summary>
+    /// <typeparam name="T">DB context type</typeparam>
+    /// <param name="services">Services collection</param>
+    /// <exception cref="InvalidOperationException">
+    /// Thrown if service descriptor cannot be found for <typeparamref name="T"/>
+    /// </exception>
+    public static void ReplacedSqlServerWithSqlLite<T>(this IServiceCollection services)
+        where T : DbContext
+    {
+        var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                    typeof(DbContextOptions<T>))
+                        ?? throw new InvalidOperationException("Could not find descriptor for DB context");
+
+        services.Remove(descriptor);
+
+        services.AddDbContext<T>(options => options.UseSqlite($"DataSource={typeof(T).Name}-{Guid.NewGuid()}.db"));
+    }
+}
