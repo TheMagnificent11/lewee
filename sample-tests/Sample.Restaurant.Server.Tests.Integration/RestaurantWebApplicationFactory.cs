082af62bf6af3a62ff1c5ff65ef43765108fe995
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-03-20 19:04:06 +1000

    failed attempt to fix db connection

--- sample-tests/Sample.Restaurant.Server.Tests.Integration/RestaurantWebApplicationFactory.cs
+++ sample-tests/Sample.Restaurant.Server.Tests.Integration/RestaurantWebApplicationFactory.cs
@@ -1,7 +1,6 @@
-﻿using Lewee.IntegrationTests;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Sample.Restaurant.Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace Sample.Restaurant.Server.Tests.Integration;
 
@@ -9,6 +8,12 @@ public class RestaurantWebApplicationFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        builder.ConfigureServices(services => services.ReplacedSqlServerWithSqlLite<RestaurantDbContext>());
+        var projectDir = Directory.GetCurrentDirectory();
+        var configPath = Path.Combine(projectDir, "appsettings.json");
+
+        builder.ConfigureAppConfiguration((_, config) =>
+        {
+            config.AddJsonFile(configPath);
+        });
     }
 }
