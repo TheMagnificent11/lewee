529e9035c6d988aca6752c6566b021be901e2fbb
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-06-14 05:09:15 +1000

    exclude migrations from code coverage

--- sample/Sample.Restaurant.Infrastructure/Migrations/20230114091637_MenuItems.cs
+++ sample/Sample.Restaurant.Infrastructure/Migrations/20230114091637_MenuItems.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -7,6 +8,7 @@
 namespace Sample.Restaurant.Infrastructure.Migrations;
 
 /// <inheritdoc />
+[ExcludeFromCodeCoverage]
 public partial class MenuItems : Migration
 {
     /// <inheritdoc />
--- sample/Sample.Restaurant.Infrastructure/Migrations/20230225024117_DomainEventReferencesClientIdColumn.cs
+++ sample/Sample.Restaurant.Infrastructure/Migrations/20230225024117_DomainEventReferencesClientIdColumn.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Sample.Restaurant.Infrastructure.Migrations;
 
 /// <inheritdoc />
+[ExcludeFromCodeCoverage]
 public partial class DomainEventReferencesClientIdColumn : Migration
 {
     /// <inheritdoc />
