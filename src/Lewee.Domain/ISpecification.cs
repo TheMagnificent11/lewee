d105aeef4d9ed8950be5fcbacbfa9c033db2d177
Saji Weerasingham <TheMagnificent11@users.noreply.github.com>
2023-09-02 06:13:57 +1000

    #41 `ISpecification` (#42)

---
+++ src/Lewee.Domain/ISpecification.cs
@@ -0,0 +1,15 @@
+﻿namespace Lewee.Domain;
+
+/// <summary>
+/// Specification Interface
+/// </summary>
+/// <typeparam name="T">Type to validate</typeparam>
+public interface ISpecification<T>
+{
+    /// <summary>
+    /// Determines whether the <paramref name="input"/> satifies the this specification/rule
+    /// </summary>
+    /// <param name="input">Input to validate</param>
+    /// <returns>True if valid, otherwise false</returns>
+    bool IsValid(T input);
+}
