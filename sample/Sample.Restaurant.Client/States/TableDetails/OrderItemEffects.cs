52b6939f93603ffb5cbd42a7162945a52975bfcb
Saji Weerasingham <saji.weerasingham@outlook.com>
2023-09-19 23:47:21 +1000

    Refactored `TableDetailEffects` into multiple classes

---
+++ sample/Sample.Restaurant.Client/States/TableDetails/OrderItemEffects.cs
@@ -0,0 +1,51 @@
+﻿using Correlate;
+using Fluxor;
+using Lewee.Blazor.ErrorHandling;
+using Lewee.Blazor.Fluxor;
+using Lewee.Shared;
+using Sample.Restaurant.Client.States.TableDetails.Actions;
+
+namespace Sample.Restaurant.Client.States.TableDetails;
+
+public sealed class OrderItemEffects
+    : RequestEffects<TableDetailsState, OrderItemAction, OrderItemSuccessAction, OrderItemErrorAction>
+{
+    private readonly ITableClient tableClient;
+
+    public OrderItemEffects(
+        IState<TableDetailsState> state,
+        ITableClient tableClient,
+        ICorrelationContextAccessor correlationContextAccessor,
+        ILogger<OrderItemEffects> logger)
+        : base(state, correlationContextAccessor, logger)
+    {
+        this.tableClient = tableClient;
+    }
+
+    [EffectMethod]
+    public Task OrderItemCompleted(OrderItemCompletedAction action, IDispatcher dispatcher)
+    {
+        using (this.Logger.BeginScope(LoggingConsts.CorrelationId, action.CorrelationId.ToString()))
+        {
+            this.Logger.LogDebug("Received item added message from server");
+
+            dispatcher.Dispatch(new GetTableDetailsAction(action.CorrelationId, action.TableNumber));
+
+            return Task.CompletedTask;
+        }
+    }
+
+    protected override async Task ExecuteRequest(OrderItemAction action, IDispatcher dispatcher)
+    {
+        try
+        {
+            await this.tableClient.OrderMenuItemAsync(action.TableNumber, action.MenuItemId);
+            dispatcher.Dispatch(new OrderItemSuccessAction(action.CorrelationId));
+        }
+        catch (ApiException ex)
+        {
+            ex.Log(this.Logger);
+            dispatcher.Dispatch(new OrderItemErrorAction(ex.Message, action.CorrelationId));
+        }
+    }
+}
---
+++ sample/Sample.Restaurant.Client/States/TableDetails/RemoveItemEffects.cs
@@ -0,0 +1,51 @@
+﻿using Correlate;
+using Fluxor;
+using Lewee.Blazor.ErrorHandling;
+using Lewee.Blazor.Fluxor;
+using Lewee.Shared;
+using Sample.Restaurant.Client.States.TableDetails.Actions;
+
+namespace Sample.Restaurant.Client.States.TableDetails;
+
+public sealed class RemoveItemEffects
+    : RequestEffects<TableDetailsState, RemoveItemAction, RemoveItemSuccessAction, RemoveItemErrorAction>
+{
+    private readonly ITableClient tableClient;
+
+    public RemoveItemEffects(
+        IState<TableDetailsState> state,
+        ITableClient tableClient,
+        ICorrelationContextAccessor correlationContextAccessor,
+        ILogger<RemoveItemEffects> logger)
+        : base(state, correlationContextAccessor, logger)
+    {
+        this.tableClient = tableClient;
+    }
+
+    [EffectMethod]
+    public Task RemoveItemCompleted(RemoveItemCompletedAction action, IDispatcher dispatcher)
+    {
+        using (this.Logger.BeginScope(LoggingConsts.CorrelationId, action.CorrelationId.ToString()))
+        {
+            this.Logger.LogDebug("Received item removed message from server");
+
+            dispatcher.Dispatch(new GetTableDetailsAction(action.CorrelationId, action.TableNumber));
+
+            return Task.CompletedTask;
+        }
+    }
+
+    protected override async Task ExecuteRequest(RemoveItemAction action, IDispatcher dispatcher)
+    {
+        try
+        {
+            await this.tableClient.RemoveMenuItemAsync(action.TableNumber, action.MenuItemId);
+            dispatcher.Dispatch(new RemoveItemSuccessAction(action.CorrelationId));
+        }
+        catch (ApiException ex)
+        {
+            ex.Log(this.Logger);
+            dispatcher.Dispatch(new RemoveItemErrorAction(ex.Message, action.CorrelationId));
+        }
+    }
+}
