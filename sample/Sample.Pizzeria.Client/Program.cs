2506eefcec228d7c72a16b4f749662502fb463c0
Saji Weerasingham <saji.weerasingham@outlook.com>
2024-02-29 21:36:23 +1000

    wip

--- sample/Sample.Pizzeria.Client/Program.cs
+++ sample/Sample.Pizzeria.Client/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Refit;
 using Sample.Pizzeria.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,9 +9,13 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
-builder.Services.AddTransient<AuthorizationMessageHandler>();
-builder.Services.AddRefitClient<IOrdersApi>()
-    .ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+//builder.Services.AddRefitClient<IOrdersApi>()
+//    .ConfigureHttpClient(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+//    .AddHttpMessageHandler<AuthorizationMessageHandler>();
+builder.Services
+    .AddSingleton<IOrdersApi, OrdersHttpClient>()
+    .AddSingleton<AuthorizationMessageHandler>()
+    .AddHttpClient<IOrdersApi, OrdersHttpClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<AuthorizationMessageHandler>();
 
 await builder.Build().RunAsync();
