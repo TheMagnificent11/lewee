dffc1f25e0b9f658386bf7dfa9af9ad57d5d9a2d
Saji Weerasingham <saji.weerasingham@outlook.com>
2024-03-17 08:55:34 +1000

    wip

---
+++ sample/Sample.Pizzeria.Application/AddPizzaToOrderCommand.cs
@@ -0,0 +1,58 @@
+﻿using Lewee.Application.Mediation.Requests;
+using Lewee.Domain;
+using MediatR;
+using Sample.Pizzeria.Domain;
+using Serilog;
+
+namespace Sample.Pizzeria.Application;
+
+public sealed class AddPizzaToOrderCommand : ICommand
+{
+    public AddPizzaToOrderCommand(Guid orderId, int pizzaId, Guid correlationId)
+    {
+        this.OrderId = orderId;
+        this.PizzaId = pizzaId;
+        this.CorrelationId = correlationId;
+    }
+
+    public Guid OrderId { get; }
+    public int PizzaId { get; }
+    public Guid CorrelationId { get; }
+
+    internal class AddPizzaToOrderCommandHandler : IRequestHandler<AddPizzaToOrderCommand, CommandResult>
+    {
+        private readonly IRepository<Order> orderRepository;
+        private readonly ILogger logger;
+
+        public AddPizzaToOrderCommandHandler(IRepository<Order> orderRepository, ILogger logger)
+        {
+            this.orderRepository = orderRepository;
+            this.logger = logger.ForContext<AddPizzaToOrderCommandHandler>();
+        }
+
+        public async Task<CommandResult> Handle(AddPizzaToOrderCommand request, CancellationToken cancellationToken)
+        {
+            var pizza = Menu.Pizzas.FirstOrDefault(x => x.Id == request.PizzaId);
+            if (pizza == null)
+            {
+                this.logger.Warning("Invalid Pizza ID {PizzaId}", request.PizzaId);
+                return CommandResult.Fail(ResultStatus.BadRequest, "Pizza not found on menu");
+            }
+
+            var order = await this.orderRepository.RetrieveById(request.OrderId, cancellationToken);
+            if (order == null)
+            {
+                this.logger.Warning("Invalid Order ID {OrderId}", request.OrderId);
+                return CommandResult.Fail(ResultStatus.NotFound, "Order not found");
+            }
+
+            order.AddPizza(pizza, request.CorrelationId);
+
+            await this.orderRepository.SaveChanges(cancellationToken);
+
+            this.logger.Information("Pizza {PizzaId} was added to Order {OrderId}", request.PizzaId, request.OrderId);
+
+            return CommandResult.Success();
+        }
+    }
+}
---
+++ sample/Sample.Pizzeria/Endpoints/AddPizzaToOrderEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using FastEndpoints;
+using Lewee.Infrastructure.AspNet.WebApi;
+using Sample.Pizzeria.Application;
+
+namespace Sample.Pizzeria.Endpoints;
+
+public sealed class AddPizzaToOrderEndpoint : CommandEndpoint<EmptyRequest>
+{
+    protected override string Route => "orders/{orderId}/pizzas/{pizzaId}";
+
+    protected override CommandType CommandType => CommandType.Post;
+
+    protected override string Name => "AddPizzaToOrder";
+
+    protected override bool IsAnonymousAllowed => false;
+
+    public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+    {
+        var command = new AddPizzaToOrderCommand(
+            this.Route<Guid>("orderId"),
+            this.Route<int>("pizzaId"),
+            this.CorrelationId);
+
+        var result = await this.Mediator.Send(command, ct);
+
+        await this.ToResponse(result, ct);
+    }
+}
