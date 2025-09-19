39b6d7006ff5afd76094658f1e68711ad2163383
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-09-19 21:57:50 +1000

    revert

--- sample/Sample.Restaurant.Common/ServiceNames.cs
+++
@@ -1,8 +0,0 @@
-namespace Sample.Restaurant.Common;
-
-public static class ServiceNames
-{
-    public const string Postgres = "postgres";
-    public const string RestaurantDb = "restaurant-db";
-    public const string RestaurantApi = "restaurant-api";
-}
