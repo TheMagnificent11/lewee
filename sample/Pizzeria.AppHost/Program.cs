5b19ed33314538637dd9ad9627980c1386b13c63
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-10-07 04:18:42 +1000

    Disable sample web client

--- sample/Pizzeria.AppHost/Program.cs
+++ sample/Pizzeria.AppHost/Program.cs
@@ -12,13 +12,27 @@ var databaseServer = Environments.IsIntegrationTesting
 var pizzaStoreDatabaseName = ServiceNames.GetPizzaStoreDatabaseName();
 var pizzaStoreDatabase = databaseServer.AddDatabase(pizzaStoreDatabaseName);
 
-var pizzaStoreApi = builder.AddProject<Projects.Pizzeria_Store_Api>(ServiceNames.PizzaStoreApi)
+builder.AddProject<Projects.Pizzeria_Store_Api>(ServiceNames.PizzaStoreApi)
     .WithReference(pizzaStoreDatabase)
-    .WaitFor(pizzaStoreDatabase);
+    .WaitFor(pizzaStoreDatabase)
+    .WithHttpHealthCheck("/health");
 
+/*
+ * Saji (202510-07)
+ * --------------------------------------------------------------------------------------
+ * The server-rendered Blazor app has a bug receiving SignalR messages.
+ *
+ * The preference is to use Blazor WASM for the web client project, but there is bug with
+ * Blazor WASM Aspire service discovery (https://github.com/dotnet/aspire/issues/8486).
+ *
+ * Re-enable this when both of the above issues are fixed.
+ * https://github.com/TheMagnificent11/lewee/issues/372
+ *
 builder.AddProject<Projects.Pizzeria_Store_WebClient>(ServiceNames.PizzaStoreWebClient)
     .WithReference(pizzaStoreApi)
-    .WaitFor(pizzaStoreApi);
+    .WaitFor(pizzaStoreApi)
+    .WithHttpHealthCheck("/health");
+*/
 
 var app = builder.Build();
 
