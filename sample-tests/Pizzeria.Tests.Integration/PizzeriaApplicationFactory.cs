57f0db46d5230ac35266c32bb212b0f9bf8ef185
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-10-13 15:55:22 +1000

    Don't add resilience handlers for integration tests

--- sample-tests/Pizzeria.Tests.Integration/PizzeriaApplicationFactory.cs
+++ sample-tests/Pizzeria.Tests.Integration/PizzeriaApplicationFactory.cs
@@ -31,10 +31,7 @@ public sealed class PizzeriaApplicationFactory : IAsyncLifetime
 
         // https://learn.microsoft.com/en-us/dotnet/aspire/testing/manage-app-host?pivots=xunit
         this.builder = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Pizzeria_AppHost>();
-        this.builder.Services.ConfigureHttpClientDefaults(x =>
-        {
-            x.AddStandardResilienceHandler();
-        });
+        this.builder.Services.ConfigureHttpClientDefaults(x => { });
 
         this.app = await this.builder.BuildAsync();
         this.resourceNotificationService = this.app.Services.GetRequiredService<ResourceNotificationService>();
