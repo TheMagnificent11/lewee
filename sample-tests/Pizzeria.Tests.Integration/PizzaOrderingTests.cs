93ae3ceb9c6aad040c37504205879e60c6a23aa1
Saji Weerasingham <saji.weerasingham@outlook.com>
2025-10-29 04:10:08 +1000

    Skip integration tests that require Keycloak

--- sample-tests/Pizzeria.Tests.Integration/PizzaOrderingTests.cs
+++ sample-tests/Pizzeria.Tests.Integration/PizzaOrderingTests.cs
@@ -13,7 +13,7 @@ public sealed class PizzaOrderingTests : PizzeriaTests
     {
     }
 
-    [Fact]
+    [Fact(Skip = "Keycloak is not correctly configured")]
     public async Task Should_CreateOrder_When_OrderIsPlacedAsync()
     {
         // Arrange
@@ -39,7 +39,7 @@ public sealed class PizzaOrderingTests : PizzeriaTests
         orderProjection.Order.Id.Should().Be(order.Id);
     }
 
-    [Fact]
+    [Fact(Skip = "Keycloak is not correctly configured")]
     public async Task Should_AddPizzaToOrder_When_PizzaIsAddedAsync()
     {
         // Arrange
