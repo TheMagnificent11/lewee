d0528c7656248d3ca6e9dbbaea6a3a0d78a66874
TheMagnificent11 <saji.weerasingham@outlook.com>
2026-01-09 01:55:05 +1000

    Remove unused project

--- tests/Lewee.Infrastructure.Data.Tests.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-using Lewee.Infrastructure.Data.Tests.AppHost;
-
-var builder = DistributedApplication.CreateBuilder(args);
-
-builder
-    .AddPostgres(ServiceNames.DatabaseServer)
-    .AddDatabase(ServiceNames.Database);
-
-await builder.Build().RunAsync();
